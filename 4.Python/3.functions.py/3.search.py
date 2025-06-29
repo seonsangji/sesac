b9793343cd677c8d9fe1a302355daece6be01719
seonsangji <pongjja01@gmail.com>
2025-06-29 22:27:18 +0900

    python 200 prac

--- 4.Python/3.functions.py/3.search.py
+++ 4.Python/3.functions.py/3.search.py
@@ -1,7 +1,9 @@
 numbers = [1,3,5,7,9,11,13,15,17,19]
 def linear_search(numbers,target):
+    count = 0
     for i in range(len(numbers)):
         if numbers[i] ==target:
+            print("선형탐색에서의 비교횟수: ", count)
             return i
         
     return -1
@@ -23,5 +25,6 @@ def binary_search(numbers,target):
 
 target = 17
 print(linear_search(numbers,target))
+print(binary_search(numbers,target))
 
 
