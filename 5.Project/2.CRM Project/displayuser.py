0a13c498168e4d52f6358579f8855517a4a39f84
seonsangji <pongjja01@gmail.com>
2025-07-03 10:36:29 +0900

    data generator : user.csv 파일 만들기

--- 5.Project/2.CRM Project/displayuser.py
+++ 5.Project/2.CRM Project/displayuser.py
@@ -1,4 +1,5 @@
 from userclass import GenerateUser
+import sys
 
 
 class DisplayData(GenerateUser):
@@ -9,9 +10,48 @@ class DisplayData(GenerateUser):
 
     def printCSV(self,count):
         data = self.generateUser(count)
+        
         print("csv에 저장하기")
+        return data
 
 
-a = DisplayData()
-a.printData(1)
+# a = DisplayData()
+# # a.printData(2)
+# print(a.printCSV(2))
 
+class PrintCSV:
+
+    userNum = int(input("user수: "))
+    
+
+    generateInputUser = DisplayData()
+    # generateInputUser.printCSV(userNum)
+    userData = generateInputUser.printCSV(userNum)
+
+# print(userData)
+
+
+
+# input 에서 count값 (user 몇명 만들지) 받고 - userNum
+# generateUser() : count값만큼 user 생성
+# printCSV() : [( , , , , ), ( , , , , )] 형태의 data를 데리고 csv 만들고 write.
+
+    
+        
+    import csv
+
+    with open ( "users.csv", "w", newline="", encoding="utf-8") as file:
+        print(userData)
+        for i in range(len(userData)):
+            print(userData[i]) 
+
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(["id", "이름", "성별", "생년월일", "나이", "주소"])
+        csv_writer.writerows(userData)
+    
+
+
+    
+
+    
+    # 이때 새 csv 만들기 / 기존 csv에 추가하기로 나누자
