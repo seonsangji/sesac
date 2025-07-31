bc4b0e8a380af3c766cabc4b07251d9cdf2f2a0c
seonsangji <pongjja01@gmail.com>
2025-07-31 16:46:35 +0900

    [BOOTSTRAP] fetch를 동기방식으로 바꾸기 및 bootstrap 디자인

---
+++ 6.Flask/8.flash/2.design_bootstrap/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+app = Flask(__name__)
+app.secret_key = 'yummm'
+
+users = [
+    {'name':'aa', 'id': 'user', 'pw': 'password'}
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        user = request.form.get('id')
+        password = request.form.get('password')
+        user = next((u for u in users if u['id'] == user and u['pw'] == password), None)
+        if user:
+            session['user'] = user
+            flash("로그인에 성공했습니다", "success")
+            return redirect(url_for('user'))
+        flash("ID 또는 PW 가 일치하지 않습니다","danger")
+        return redirect(url_for('home'))
+    if 'user' in session:
+        flash("이미 로그인 된 사용자입니다.", "warning")
+        return redirect(url_for('user'))
+    return redirect(url_for('home'))
+
+@app.route('/user')
+def user():
+    if 'user' in session:
+        user = session.get('user')
+        flash("이미 로그인 된 사용자입니다", "warning")
+        return render_template('dashboard.html', name=user['name'])
+    
+    flash("로그인부터 하세요 ㅡㅡ")
+    return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    if 'user' in session:
+        session.pop('user', None)
+        flash("정상적으로 로그아웃 되었습니다", "success")
+    else: 
+        flash("이미 로그아웃된 사용자입니다.", "warning")
+    return redirect(url_for('home'))
+ 
+
+if __name__ == '__main__':
+    app.run(debug=True)
